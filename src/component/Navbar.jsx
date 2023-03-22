e7db9ce340c1828c4e1fe1a016965ef74886a149
Nikunjkanteliya <kanteliyanikunj422@gmail.com>
2023-03-22 15:59:50 +0530

    5

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import  style from "./style/nav.module.css"
+import style from "./style/nav.module.css";
 const Navbar = () => {
   return (
     <div>
       <div className={style.mainheadnav}>
         <h3> Nikunj Kanteliya</h3>
-        
-          <ul className={style.ulist} >
-            <li className={style.llist}>Home</li>
-            <li className={style.llist}>About</li>
-            <li className={style.llist}>Skills</li>
-            <li className={style.llist}>Projects</li>
-            <li className={style.llist}>Contact</li>
-          </ul>
-        
+
+        <ul className={style.ulist} style={{ listStyleType: "none" }}>
+          <li className={style.llist}>Home</li>
+          <li className={style.llist}>About</li>
+          <li className={style.llist}>Skills</li>
+          <li className={style.llist}>Projects</li>
+          <li className={style.llist}>Contact</li>
+        </ul>
       </div>
     </div>
   );
